372b70d2e9362fc73f0cddc151a57fea8e8f58b8
Vasyl Boyko <vboiko0@gmail.com>
2020-01-09 16:17:38 +0200

    add: routes

---
+++ src/views/EventList.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>Events Listing</h1>
+    <router-link :to="{ name: 'event-show', params: { id: '1' } }"
+      >Event #1</router-link
+    >
+  </div>
+</template>
