ad5aa36820f2bc70e6746be83352b6b31ffde5b7
Vasyl Boyko <vboiko0@gmail.com>
2020-01-10 14:23:23 +0200

    add: global BaseIcon component

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -6,6 +6,7 @@
     <div class="event-card -shadow">
       <span class="eyebrow">{{ event.date }} on {{ event.time }}</span>
       <h4 class="title">{{ event.title }}</h4>
+      <base-icon name="users" />
       <span>{{ event.attendees.length }} attending</span>
     </div>
   </router-link>
